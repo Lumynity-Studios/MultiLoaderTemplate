f29d990068c3c487cc0607c97671287a865e0dc9
JustMili <rodx160@gmail.com>
2026-07-28 20:19:04 +0200

    Add logger and change MOD_ID to MODID

--- common/src/main/java/net/lumynitystudios/examplemod/ExampleMod.java
+++ common/src/main/java/net/lumynitystudios/examplemod/ExampleMod.java
@@ -3,11 +3,12 @@ package net.lumynitystudios.examplemod;
 import net.minecraft.resources.ResourceLocation;
 
 public class ExampleMod {
-    public static final String MOD_ID = "examplemod";
+    public static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(ExampleMod.class);
+    public static final String MODID = "examplemod";
 
     public static void init() {}
 
     public static ResourceLocation asResource(String name) {
-        return new ResourceLocation(MOD_ID, name);
+        return new ResourceLocation(MODID, name);
     }
 }
--- forge/src/main/java/net/lumynitystudios/examplemod/forge/ExampleModForge.java
+++ forge/src/main/java/net/lumynitystudios/examplemod/forge/ExampleModForge.java
@@ -6,7 +6,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(ExampleMod.MOD_ID)
+@Mod(ExampleMod.MODID)
 public final class ExampleModForge {
     public static IEventBus EVENT_BUS;
 
